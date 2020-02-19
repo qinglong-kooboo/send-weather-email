067571dc9c2c0c4f460dd50f6f1bb7ab8a1de8a8
qinglong-kooboo <40824476+qinglong-kooboo@users.noreply.github.com>
2020-02-19 19:19:38 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 set -eux
+CITY=Xian
 LANGUAGE="zh-CN"
+UNIT=m
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o result.html \
-  wttr.in/Xian_tqp0.png
+  wttr.in/$CITY_tqp0?format=4\&$UNIT

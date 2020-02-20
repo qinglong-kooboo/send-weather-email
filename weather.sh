658b88edb98b847ee6612683c02f8eca25099e9c
qinglong-kooboo <40824476+qinglong-kooboo@users.noreply.github.com>
2020-02-20 09:45:22 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -5,14 +5,9 @@ CITY=Xian
 LANGUAGE="zh-CN"
 OPTIONS=tqp0
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
-SOUCEJPG="https://bing.ioliu.cn/v1/rand?w=1024&h=768"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-convert \
-  $SOUCEJPG <( wttr.in/$CITY\_$OPTIONS.png ) -geometry +50+50 -composite target.jpg
-wget \
-  -p target.jpg
-curl \
-  -o result.html 
+  -o result.html \
+  wttr.in/$CITY\_$OPTIONS.png

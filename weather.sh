6e9c1473d02df86336410336d7fa5c9465536784
qinglong-kooboo <40824476+qinglong-kooboo@users.noreply.github.com>
2020-02-20 13:25:40 +0800

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -10,6 +10,6 @@ OPTIONS=tqp0
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
-  -o result.html ./background.html
+  -o result.html wttr.in/$CITY?format=1\&$UNIT
   #wttr.in/$CITY\_$OPTIONS.png
   #wttr.in/$CITY?format=1\&$UNIT

2c7f4dccceebbca9775d73de82a78974cedc4381
qinglong <qinglong@kooboo.cn>
2020-02-28 15:22:05 +0800

    Update sendgrid.js

--- sendgrid.js
+++ sendgrid.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const msg = {
     to: 'wu_thor@foxmail.com',
-    from: 'wqlthor@163.com',
+    from: process.env.SENDER_MAIL_USERNAME,
     subject: 'Hello world',
     text: 'Hello plain world!',
     html: '<p>Hello HTML world</p>',
